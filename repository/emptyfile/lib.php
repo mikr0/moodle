e27373ea3cf5d34fe793cd2a64366a7d113e25e3
Charles YVANES <charles.yvanes@enovationsolutions.fr>
2018-04-24 17:45:05 +0200

    added logic if file already exists, still need to display it so we can select it

--- repository/emptyfile/lib.php
+++ repository/emptyfile/lib.php
@@ -15,7 +15,26 @@ class repository_emptyfile extends repository {
             'filepath' => '/'
         );
 
-        $fs->create_file_from_string($filerecord, "");
+        if (!$fs->file_exists(
+            $filerecord['contextid'],
+            $filerecord['component'],
+            $filerecord['filearea'],
+            $filerecord['itemid'],
+            $filerecord['filepath'],
+            $filerecord['filename']
+        )) {
+            $fs->create_file_from_string($filerecord, "");
+        } else {
+            $myemptyfile = $fs->get_file($filerecord['contextid'],
+                $filerecord['component'],
+                $filerecord['filearea'],
+                $filerecord['itemid'],
+                $filerecord['filepath'],
+                $filerecord['filename']
+            );
+        }
+
+        //TODO: find a way to inject $myemptyfile into $list so we can select it
 
         if (empty($list['list'])) {
             $list['list'] = array();
